8a4e42bb9a744d7b9ebcd44638a6767e4847d776
sanujasethi06 <sethisanuja1998@gmail.com>
2024-02-13 12:30:12 +0530

    Footer section

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import dynamic from "next/dynamic";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,6 +36,7 @@ export default function RootLayout({
           
         <Header/>
             {children}
+            <Footer/>
             <Toaster position="top-right"/>
         </ActiveSectionContextProvider>
         </ThemeContextProvider>
